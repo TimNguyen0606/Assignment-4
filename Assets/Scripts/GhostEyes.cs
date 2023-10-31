0ff5fce76cbac8ae63e7cc99908b808847e80d9b
TimNguyen0606 <ChiThien.Nguyen-1@student.uts.edu.au>
2023-10-31 20:59:40 +0300

    changes

--- Assets/Scripts/GhostEyes.cs
+++ Assets/Scripts/GhostEyes.cs
@@ -4,45 +4,41 @@ using UnityEngine;
 
 public class GhostEyes : MonoBehaviour
 {
-    private SpriteRenderer sr;   // Reference to the SpriteRenderer component
-    private Movement movement;   // Reference to the Movement component
+    private SpriteRenderer sr;
+    private Movement movement;
 
     [SerializeField]
-    private Sprite up;   // Sprite for the "up" direction
+    private Sprite up;
     [SerializeField]
-    private Sprite down; // Sprite for the "down" direction
+    private Sprite down;
     [SerializeField]
-    private Sprite left; // Sprite for the "left" direction
+    private Sprite left;
     [SerializeField]
-    private Sprite right; // Sprite for the "right" direction
+    private Sprite right;
 
     private void Awake()
     {
-        // Get the SpriteRenderer component attached to this object
-        sr = GetComponent<SpriteRenderer();
-
-        // Get the Movement component from the parent object
+        sr = GetComponent<SpriteRenderer>();
         movement = GetComponentInParent<Movement>();
     }
 
     private void Update()
     {
-        // Update the sprite displayed on the GhostEyes based on the current movement direction
         if (movement.direction == Vector2.up)
         {
-            sr.sprite = up; // Set the sprite to the "up" sprite
+            sr.sprite = up;
         }
         else if (movement.direction == Vector2.down)
         {
-            sr.sprite = down; // Set the sprite to the "down" sprite
+            sr.sprite = down;
         }
         else if (movement.direction == Vector2.left)
         {
-            sr.sprite = left; // Set the sprite to the "left" sprite
+            sr.sprite = left;
         }
         else if (movement.direction == Vector2.right)
         {
-            sr.sprite = right; // Set the sprite to the "right" sprite
+            sr.sprite = right;
         }
     }
 }
